02ee1a0e866df7c306a50c59fd0bc6d7292be32a
jose caldeira <j.caldeira@diatomea.tech>
2023-09-13 23:38:54 +0200

    search by email!

--- src/components/LayoutCards.tsx
+++ src/components/LayoutCards.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { useState, memo } from 'react';
 import { Result } from '../interface';
 import { Card } from './';
 
@@ -6,21 +6,37 @@ interface Props {
     data: Result[];
 }
 
+function useInput({ type }) {
+    const [value, setValue] = useState('');
+    const input = <input value={value} onChange={e => setValue(e.target.value)} type={type} />;
+    return [value, input];
+}
+
 export const LayoutCards = memo(({ data }: Props) => {
+    console.log(data);
+    const filter = 'com';
+    const [email, emailInput] = useInput({ type: 'text' });
+
     return (
-        <div className="container-cards">
-            {data.length > 0 &&
-                data
-                    .sort(function(a, b) {
-                        if (a['first name'] < b['first name']) {
-                            return -1;
-                        }
-                        if (a['first name'] > b['first name']) {
-                            return 1;
-                        }
-                        return 0;
-                    })
-                    .map(User => <Card {...User} key={User.id} />)}
+        <div>
+            <p className="font">Search by email! {emailInput} </p>
+            <div className="container-cards">
+                {data.length > 0 &&
+                    data
+                        .sort(function(a, b) {
+                            if (a['first name'] < b['first name']) {
+                                return -1;
+                            }
+                            if (a['first name'] > b['first name']) {
+                                return 1;
+                            }
+                            return 0;
+                        })
+                        .filter(function(user) {
+                            return user.email.toLowerCase().includes(email.toLowerCase());
+                        })
+                        .map(user => <Card {...user} key={user.id} />)}
+            </div>
         </div>
     );
 });
